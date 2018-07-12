b8cbf32edf09b2712991e6828be705825d7c1561
Naquaiviera Willis <naquaiviera.willis@gmail.com>
2018-07-12 14:09:55 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -10,22 +10,22 @@ puts "Step 1: Right arm out in front of you, palm facing down."
  end
 # Step 2: FIXME
 puts "Step 2: Left arm out in front of you, plam facing down."
-end
+
 # Step 3: FIXME
 puts "Step 3: Turn your right palm up."
-end
+
 # Step 4: FIXME
 puts "Step 4: Turn your left palm up."
-end
+
 # Step 5: FIXME
 puts "Step 5: Place your right hand on your left shoulder."
-end
+
 # Step 6: FIXME
 puts "Step 6: place your left hand on your right shoulder."
-end
+
 # Step 7: FIXME
 puts "Step 7: bring your right hand up and around to the back of your head."
-end
+
 # Step 8: FIXME
 puts "Step 8: Bring your left hand up and around to the back of your head"
  end
